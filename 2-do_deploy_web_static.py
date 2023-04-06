2aee521b8ab9267adf9dcb31ca2cf647562bae8b
obaraelijah <elijahobara357@gmail.com>
2023-04-06 15:24:34 +0000

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy
 
-from fabric.api import *
-import os
+from fabric.api import run, env, put
+import os.path
 
 env.hosts = ['34.207.253.223', '100.25.135.33']
 env.filename_path = '~/.ssh/school'
