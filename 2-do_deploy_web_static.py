425337f62156366b77fd9e2adbffabddb2e4eb82
obaraelijah <elijahobara357@gmail.com>
2023-04-06 17:41:11 +0000

    Deploying static files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import run, env, put
 import os.path
 
 env.hosts = ['34.207.253.223', '100.25.135.33']
-env.filename_path = '~/.ssh/school'
+env.key_filename = '~/.ssh/school'
 env.user = 'ubuntu'
 
 def do_deploy(archive_path):
@@ -23,6 +23,9 @@ def do_deploy(archive_path):
         run("sudo mkdir -p {}".format(remote_path))
         run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_filename, remote_path))
         run("sudo rm /tmp/{}".format(compressed_filename))
+        run("sudo mv {}/web_static/* {}".format(remote_path))
+        run("sudo rm -rf {}/web_static".format(remote_path))
+        run("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {} {}".format(remote_path, symbol_link))
         return True
 
