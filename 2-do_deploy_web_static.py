f38ed58f2d30f0bb3bcb89824747d8d2dde7d1dd
obaraelijah <elijahobara357@gmail.com>
2023-04-06 15:10:31 +0000

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy
+
+from fabric.api import *
+import os
+
+env.hosts = ['34.207.253.223', '100.25.135.33']
+env.filename_path = '~/.ssh/school'
+env.user = 'ubuntu'
+
+def do_deploy(archive_path):
+    """Deploys and decompresses code"""
+
+    if not os.path.isfile(archive_path):
+        return False
+    compressed_filename = archive_path.split("/")[-1]
+    no_extension_filename = compressed_filename.split(".")[0]
+
+    try:
+        remote_path = "/data/web_static/releases/{}/".format(no_extension_filename)
+        symbol_link = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("sudo mkdir - p {}".format(remote_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_filename, remote_path))
+        run("sudo rm /tmp/{}".format(compressed_filename))
+        run("sudo ln -sf {} {}".format(remote_path, symbol_link))
+        return True
+
+    except Exception as e:
+        return False
+
