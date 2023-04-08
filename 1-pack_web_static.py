a8e82a67dbdb8c052be7f34776f7292d0175bea2
obaraelijah <elijahobara357@gmail.com>
2023-04-08 07:04:56 +0000

    Hot fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,21 +8,21 @@ from datetime import datetime
 def do_pack():
     """ a method to compress a file and return it's path """
 
-    """saving the current timestamp and creatinf filename"""
+    #saving the current timestamp and creatinf filename
     time_str = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_{}.tgz".format(time_str)
 
     try:
-        """create a directory called versions"""
+        #creates  directory called versions
         local("mkdir -p versions")
 
-        """create an archive file"""
+        #create an archive file
         local("tar -cvzf {} web_static/".format(file_path))
 
-        """return the path to the archive file created"""
+        #return the path to the archive file created
         return "{}".format(file_path)
 
-        """return none if an error occurs"""
+        #return none if an error occurs
     except Exception as e:
         return None
 
