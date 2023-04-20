d606fe6588af941988dfcc6821f2f90d76129dda
elijah obara <elijahobara357@gmail.com>
2023-04-20 14:58:02 +0300

    a simple flask start code

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,6 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', strict_slashes=False)
+def index():
+    """Returns hello HBNB!"""
+    return 'Hello, HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def Hbnb_route():
