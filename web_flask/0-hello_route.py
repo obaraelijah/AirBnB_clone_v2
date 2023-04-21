b45ca6cf4a3f285436f166723e490d6cca1b01ff
elijah obara <elijahobara357@gmail.com>
2023-04-21 07:54:47 +0300

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,15 +5,14 @@ from flask import Flask
 
 
 app = Flask(__name__)
-'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
-    '''The  hello hbnb page'''
+    '''The home page.'''
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000')
